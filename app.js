283863b8d74a83122b4a706c0986a4b5c8e8daaa
GopalDas123 <Gopaldax@gmail.com>
2020-06-14 17:42:33 +0500

    Go

---
+++ app.js
@@ -0,0 +1,136 @@
+// Chapter 1 --Task 1
+
+// alert("Hello User Welcome");
+
+// Chapter 1 --Task 2
+
+// alert("Error! PLease enter a valid password");
+
+// Chapter 1 --Task 3
+
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+// Chapter 1 --Task 4
+
+// alert("Welcome to JS Land..");
+// alert("Happy Coding");
+
+
+// Chapter 2 --Task 1
+
+// var username="Gopal Das";
+
+// Chapter 2 --Task 2
+
+// var myName="Gopal";
+// var str=myName+" Das";
+// alert(str);
+
+// Chapter 2 --Task 3
+// var  message="Hello World";
+// alert(message);
+
+// Chapter 2 --Task 4
+// var name="";
+// var age="";
+// var ID="";
+// name=prompt("Enter your name: ");
+// age=prompt("Enter your age: ");
+// ID=prompt("Enter your ID: ");
+// alert("Hello "+name);
+// alert("Your Age is "+age);
+// alert("& your id is "+ID);
+
+// Chapter 2 --Task 5
+// var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza)
+
+// Chapter 2 --Task 6
+// var email="pgorav11@gmail.com";
+// var a=("My Email is "+ email);
+// alert(a);
+
+// Chapter 2 --Task 7
+// var book="A smarter way to learn JavaScript";
+// alert("I'm trying to learn JavaScript from the book "+book);
+
+// Chapter 2 --Task 8
+// document.write("Yes i can write HTML content thorugh javascript")
+
+// Chapter 2 --Task 9
+// var a="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(a);
+
+
+// Chapter 3 --Task 1
+
+// var age=15;
+// alert("I'm "+age+" years old");
+
+// Chapter 3 --Task 3
+// var birthYear=1999;
+// document.write("My birthyear is " + birthYear);
+// document.write("<br>Data type of my declared variable is number");
+
+// Chapter 3 --Task 4
+// var name="";
+// var title="";
+// var quan="";
+// name=prompt("Enter your name: ");
+// title=prompt("What do you want to buy: ");
+// quan=prompt("Enter quantity: ");
+// // “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”
+// document.write([name]+" " +"ordered"+" "+ [quan]+" "+[title]+"(s)"+" on XYZ clothing store");
+
+// Chapter 5 --Task 1
+// var a="";
+// var b="";
+// a=Number(prompt("Enter 1st number: "));
+// b=Number(prompt("Enter 2nd number: "));
+// var c=a+b;
+// document.write("Sum of "+a+" & "+b+" is "+c);
+
+// Chapter 5 --Task 2
+
+// var a="";
+// var b="";
+// a=Number(prompt("Enter 1st number: "));
+// b=Number(prompt("Enter 2nd number: "));
+// var c=a-b;
+// document.write("Substraction of "+a+" & "+b+" is "+c);
+
+// Chapter 5 --Task 2
+
+// var a="";
+// var b="";
+// a=Number(prompt("Enter 1st number: "));
+// b=Number(prompt("Enter 2nd number: "));
+// var c=a*b;
+// document.write("Multiplication of "+a+" & "+b+" is "+c);
+
+// Chapter 5 --Task 2
+
+// var a="";
+// var b="";
+// a=Number(prompt("Enter 1st number: "));
+// b=Number(prompt("Enter 2nd number: "));
+// var c=a/b;
+// document.write("Division of "+a+" & "+b+" is "+c);
+
+// Chapter 5 --Task 4
+// var a=600;
+// var tickets=Number(prompt("How many tickets you want to buy?: "));
+// var price=a*tickets;
+// document.write("Total cost to buy " + tickets +"tickets to a movie is "+price);
+
+// Chapter 5 --Task 5
+// var no=Number(prompt("Enter any number: "));
+// document.write("<br> Table of "+ no );
+// for (let i = 1; i <= 10; i++) {
+//     document.write(" <br>"+i+" x "+no+" = "+i*no);   
+// }
+
+// Chapter 5 --Task 6
+// var c=10;
+// var f=(c*9/5)+32;
+
