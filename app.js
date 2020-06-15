26d53c0678825b647eda647650112df1b1b497b7
GopalDas123 <Gopaldax@gmail.com>
2020-06-15 17:08:18 +0500

    3rd Go

--- app.js
+++ app.js
@@ -148,3 +148,65 @@
 // var p=pent_count*pent;
 // var total=s+p;
 // document.write("1 Shirt's price is 800pkr <br> The price of "+shirt_count+ "shirt(s) is "+ s+"pkr."+ "<br> Pent's price is 600pkr.<br>"+"The price of "+pent_count+ "pent(s) is "+p+".<br>Your Total Bill Is: "+total+".");
+
+
+
+// Chapter 5 --Task 8
+
+// var t_marks=850;
+// var std1=730;
+// var std2=681;
+// var calc=(std1/850)*100;
+// var calc1=(std2/850)*100;
+// document.write("Total marks are "+t_marks+".<br>Student 1 got " +std1+ "Marks. His % is "+calc+".<br>");
+// document.write("Total marks are "+t_marks+".<br>Student 2 got " +std2+ "Marks. His % is "+calc1);
+
+
+
+// Chapter 5 --task 9
+
+// var val_usd=104.80;
+// var val_sr=28;
+// var usd=10;
+// var sr=25;
+// var a=usd*val_usd;
+// var b=sr*val_sr;
+// var c=a+b;
+// document.write("Total Currency in PKR: "+c)
+
+
+// Chapter 5 --Task 10
+// var a=30;
+// var t=(a+5)*10;
+// alert(t/2);
+
+
+// Chapter 5 --Task 11
+// var c_year=2020;
+// var b_year="";
+// var age="";
+// b_year=Number(prompt("Enter your birth year: "));
+// age=c_year-b_year;
+// document.write("Current year: "+c_year+".<br>Birth Year: "+b_year+".<br>Your Age is approx: "+age+".");
+
+
+// Chapter 5 --Task 12
+// var r=Number(prompt("Enter the radius: "));
+// var cc=2*3.142*r;
+// var area=3.142*r*r;
+// document.write("THE GEOMETRIZER"+"<br>The circumference is: "+cc+".<br>The area is: "+area+".");
+
+
+//Chapter 5 --Task 13
+
+// var fs= "Lays Masala";
+// var cr_age=20;
+// var exp_age=80;
+// var ttl_fs=2;
+// var total=2*365*60
+// document.write("Favourite Snack: "+fs+".<br>Current Age: "+cr_age+".<br>Esitamted Age: "+exp_age+".<br>Snacks per day: "+ttl_fs+".<br>You will need "+total+" "+fs+" to last you untill the ripe old age of "+exp_age);
+
+
+//Chapter 6 --Task 1
+
+
